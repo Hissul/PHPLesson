7cc3dd801a9125d52a2c5171aed71b3691fc7ee6
Hissul <masstadontt@rambler.ru>
2024-07-17 13:38:14 +0300

    Home work 3

--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $variable = 1;
 echo $variable;
 echo PHP_EOL;
@@ -114,3 +114,145 @@ $func = function(int $a = 0, int $b = 0): int {
 };
 
 //$fucn(1,2);
+*/
+
+
+
+
+
+/*
+Создайте новую ветку от базовой, в проекте.
+Добавьте решение следующих задач:
+
+    Создайте переменную $var со значением 100.
+
+Умножьте на 0.5
+Извлеките корень
+Приведите к int
+Выведите на экран var_dump
+*/
+
+echo("Task #1 :");
+echo ("<br>");
+
+$var = 100;
+var_dump((int)(sqrt($var*0.5)));
+
+echo ("<br>");
+echo ("<br>");
+
+
+/*
+
+    Вычислите длину вектора по координатам в переменных $x, $y, $z (Корень суммы квадратов по каждой оси)
+
+*/
+
+echo("Task #2 :");
+echo ("<br>");
+
+$x = 5;
+$y = 7;
+$z = 3;
+var_dump(sqrt(($x * 2) + ($y * 2) + ($z ** 2)));
+echo ("<br>");
+echo ("<br>");
+
+
+/*
+
+    С помощью циклов, определите, является ли строка в переменной $strPoly палиндромом.
+
+(Отдельно к буквам в строке можно обращаться как к элементам массива $strPoly[0] - первая буква )
+*/
+
+echo("Task #3 :");
+echo ("<br>");
+
+$firstStr = "deified";
+$secondStr = "morning";
+
+function Check (string $str) : string{
+
+    // $tmp = strrev($str);
+    // echo($str);
+    // echo ($tmp);
+    // if($str === $tmp){
+    //     return $str + " Yes";
+    // }
+    // return " No";
+
+
+    for($i = 0 ; $i < (int)strlen($str) / 2 ; $i++){  
+        if($str[$i] !== $str[strlen($str) - $i - 1]){
+            return $str . " " . "isn't palindrom";
+        }
+    }
+    return $str . " " . "is palindrom";
+}
+
+echo(Check($firstStr));
+echo ("<br>");
+
+echo(Check($secondStr));
+echo ("<br>");
+echo ("<br>");
+
+echo("One more way to solve that");
+echo ("<br>");
+echo ("<br>");
+
+function Check_V2(string $str) : string{
+    $tmp = strrev($str);   
+    if($str === $tmp){
+        return $str . " " . "is palindrom";
+    }
+    return $str . " " . "isn't palindrom";
+}
+echo(Check_V2($firstStr));
+echo ("<br>");
+
+echo(Check_V2($secondStr));
+echo ("<br>");
+echo ("<br>");
+
+/*
+
+    Напишите функцию, которая заполняет массив таблицы умножения (от 1 до 10) для переданного числа.
+
+Пример как должно работать
+
+$table = multiFunc(3);
+print_r($table);
+*/
+
+/*
+Array(
+	[
+		'3 * 1' => 3,
+		'3 * 2' => 6,
+		'3 * 3' => 9,
+		.....
+		'3 * 10' => 30,
+		
+	]
+)
+
+*/
+
+echo("Task #3 :");
+echo ("<br>");
+
+function multiFunc(int $number) {
+    $arr = [];
+
+    for($i = 0 ; $i < 10 ; $i++){
+        $tmp = $number . " " . "*" . ($i+1) . " " . "=" . " " . ($number * ($i+1));
+        //array_push($arr,$tmp);
+        //echo($arr[$i]);
+        $arr[$number . " " . "*" . " " . $i+1] = $number * ($i+1);
+    }
+    return $arr;
+}
+$table = multiFunc(3);
+print_r($table);
